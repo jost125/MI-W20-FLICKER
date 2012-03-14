5e0016041b7715cc0d53b0212204782e18457456
Jan Machala <machala@collabim.com>
2012-03-14 03:16:33 +0100

    Added .gitignore

--- flicker-analyzer/src/java/flickr/analyzer/api/ApiClient.java
+++ flicker-analyzer/src/java/flickr/analyzer/api/ApiClient.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package flickr.analyzer.api;
 
-/**
- *
- * @author Jan Machala <jan.machala@email.cz>
- */
 public interface ApiClient {
 	public String getPublicPhotos(String userId);
 	public String getUserId(String username);
